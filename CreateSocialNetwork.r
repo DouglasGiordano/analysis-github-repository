7bfd31805ff1bf3da57d0c1857871cf2de778533
douglas <douglasmontanhagiordano@hotmail.com>
2018-11-01 02:16:25 -0300

    Adicionado geração de vertices da rede.

--- CreateSocialNetwork.r
+++ CreateSocialNetwork.r
@@ -1,70 +1,166 @@
-create.social.network <- function(){
-  library(dplyr)
+library(dplyr)
+create.social.network <- function(name, owner){
+  library(plyr)
+  library(RMySQL)
+  library(rvest)#https://www.datacamp.com/community/tutorials/r-web-scraping-rvest use for scraping data html
+  library(purrr)#function map to get unique element html
   mydb = dbConnect(MySQL(), user='root', password='root', dbname='simple_github', host='127.0.0.1')
-  rsI = dbSendQuery(mydb, "select id, title as text, author, createdat, id as parent from issue")
+  
+  #issue
+  rsI = dbSendQuery(mydb, paste0("select  id, title as text, author, createdat, id as parent 
+                                 from issue 
+                                 where owner = '",owner,"' and name = '",name,"'"))
   issues = fetch(rsI, n=-1)
-  rsIC = dbSendQuery(mydb, "select id, bodyhtml as text, author, createdat, issue as parent from issuecomment")
+  rsIC = dbSendQuery(mydb, paste0("select id, bodyhtml as text, author, createdat, issue as parent 
+                     from issuecomment 
+                     where owner = '",owner,"' and name = '",name,"'"))
   comments = fetch(rsIC, n=-1)
-  for(row in 1:nrow(issues)){
-    comment = filter(comments, parent == issues$id[row])
+  
+  #pull
+  rsP = dbSendQuery(mydb, paste0("select id, title as text, author, createdat, id as parent 
+                    from pullrequest
+                    where owner = '",owner,"' and name = '",name,"'"))
+  pulls = fetch(rsP, n=-1)
+  rsPC = dbSendQuery(mydb, paste0("select id, bodyhtml as text, author, createdat, pull as parent 
+                     from pullcomment 
+                     where owner = '",owner,"' and name = '",name,"'"))
+  pullcomments = fetch(rsPC, n=-1)
+
+  all <- rbind(issues, pulls)
+  allcomment <- rbind(comments, pullcomments)
+  nrow.all <- nrow(all)
+  for(row in 1:nrow.all){
+    edges <- data.frame()
+    #names(edges)<-c("project_name","project_owner", "user_source", "user_target", "type", "date_time", "source")
+    comment = filter(allcomment, parent == all$id[row])
     comment$createdat = as.POSIXct(comment$createdat, "UTC", "%Y-%m-%dT%H:%M:%SZ")
-    print(nrow(comment))
-    for(i in 1:nrow(comment)){
-      # An ISO-8601 encoded UTC date string.
-      data = comment$createdat[i]
-      commentLast = filter(comment, createdat < data)
-      text = comment$text[i]
-      print(nchar(text))
-      mentions <- NULL
-
-
+    length.comment = nrow(comment)
+    if(length.comment > 1){
+      for(i in 1:length.comment){
+        # An ISO-8601 encoded UTC date string.
+        data = comment$createdat[i]
+        text = comment$text[i]
+        user = comment$author[i]
+        commentLast = filter(comment, createdat < data)
+        edge = NULL
+        
+        direct.mentions = search.direct.mention(text)
+        indirect.mentions = search.indirect.mention(text,commentLast)
+        not.mentions = search.not.mention(commentLast, direct.mentions, indirect.mentions)
+        edges = rbind(edges, get.edges.mention(comment[i,], direct.mentions, indirect.mentions, not.mentions))
+      }
     }
+    edges = na.omit(edges)
+    if(!is.null(edges) & !empty(edges)){
+      save.edges(edges, mydb)
+    }
+    message("process ",row,"/ ",nrow.all)
   }
 }
 
-#procura as menções diretas no texto do comentário em HTML
+save.edges <-function(edges, mydb){
+  values <- paste0(apply(edges, 1, function(x) paste0("('", paste0(x, collapse = "', '"), "')")), collapse = ", ")
+  query <- paste0("INSERT INTO edge (user_target,type,project_name,project_owner,date_time,source,user_source) VALUES ", values, " ON DUPLICATE KEY UPDATE user_target = user_target;")
+  dbSendQuery(mydb, query)
+}
+
+get.edges.mention <- function(comment, direct.mentions, indirect.mentions, not.mentions){
+  #https://www.pmg.com/blog/insert-r-data-frame-sql%EF%BB%BF/
+  data.frame.edges = data.frame()
+  #names(data.frame.edges)<-c("user_target","type")
+  if(!is.null(direct.mentions)){
+    for(i in 1:length(direct.mentions)){
+      user = gsub("@", "", direct.mentions[i], fixed = TRUE)
+      data.frame.edges<-rbind(data.frame.edges, data.frame(user_target = user,
+                                                           type = "D"))
+    }
+  }
+  
+  if(!is.null(indirect.mentions)){
+    for(i in 1:length(indirect.mentions)){
+      user = gsub("@", "", indirect.mentions[i], fixed = TRUE)
+      data.frame.edges<-rbind(data.frame.edges, data.frame(user_target = user,
+                                                           type = "I"))
+    }
+  }
+  
+  if(!is.null(not.mentions)){
+    for(i in 1:length(not.mentions)){
+      user = gsub("@", "", not.mentions[i], fixed = TRUE)
+      data.frame.edges<-rbind(data.frame.edges, data.frame(user_target = user,
+                                                           type = "S"))
+    } 
+  }
+  if(nrow(data.frame.edges) == 0){
+    return(NULL)
+  }
+  data.frame.edges$project_name = "name"
+  data.frame.edges$project_owner = "owner"
+  data.frame.edges$date_time = comment$createdat
+  data.frame.edges$source = comment$parent
+  data.frame.edges$user_source = comment$author
+  return(data.frame.edges)
+}
+
+search.not.mention <- function(commentLast, direct.mentions, indirect.mentions){
+  not.mentions = c()
+  if(length(direct.mentions)== 0 & length(indirect.mentions)== 0){
+    for(h in 1:nrow(commentLast)){
+      not.mentions = c(not.mentions, commentLast$author[h])
+    }
+  }
+  return(not.mentions)
+}
+
+#procura as menÃ§Ãµes diretas no texto do comentÃ¡rio em HTML
 search.direct.mention <- function(text){
   mentions = c()
-  if(grepl("user-mention", text, fixed=TRUE)){ #verifica se a issue tem alguma menção direta
-    memtions.html = read_html(text) %>% html_nodes('.user-mention') 
-    for(h in 1:nrow(memtions)){ #extrai cada uma das menções
-      mention = memtions.html[i] %>% html_text()
+  if(grepl("user-mention", text, fixed=TRUE)){
+    mentions.html = read_html(text) %>% html_nodes('.user-mention')
+    mentions.length = length(mentions.html)
+    for(i in 1:mentions.length){
+      mention = mentions.html[i] %>% html_text()
+      mentions = c(mentions,mention)
     }
   }
+  return(mentions)
 }
 
-#procura as menções indiretas no texto do comentário em HTML
-#toda mençaõ indireta é encontrada na tag HTML blockquote
+#procura as menÃ§Ãµes indiretas no texto do comentÃ¡rio em HTML
+#toda menÃ§Ã£o indireta Ã© encontrada na tag HTML blockquote
 #toda vez que um blockquote for encontrar devemos pesquisar 
-#se o texto é uma referencia a outros comentários
-search.indirect.mention <- function(text){
+#se o texto Ã© uma referencia a outros coment?rios
+search.indirect.mention <- function(text, comment){
+  mentions = c()
   if(grepl("<blockquote>", text, fixed=TRUE)){
-    res <- read_html(text) %>% html_nodes('blockquote') %>% html_text()
-    if(is.character(res)){
-      trecho <- read_html(text) %>% html_nodes('blockquote') %>% html_nodes('p')
-      
-      if(is.list(trecho)){
-        message("is list")
-        if(grepl("wrote:", trecho[1], fixed=TRUE)){
-          trecho <- trecho[2] %>%
-            html_text()
-          message(trecho)
-        } else {
-          trecho <- trecho[1] %>%
-            html_text()
-        }
-        
-        for(h in 1:nrow(comment)){
-          if(comment$id[i]!=comment$id[h]){
-            message("Texto corresponde ", grepl(trecho, comment$text[h], fixed=TRUE))
-            if(grepl(trecho, comment$text[h], fixed=TRUE)){
-              memtions <- comment$author[h]
-              message("Menção indireta: ", memtions)
-            }
-          }
+    blockquote.html = read_html(text) %>% html_nodes('blockquote')
+    p.html = blockquote.html  %>% html_nodes('p')
+    comment.p = NULL
+    if(length(p.html) != 0){
+      if(grepl("wrote:", p.html[1], fixed=TRUE)){
+        comment.p <- p.html[2] %>% html_text()
+      } else {
+        comment.p <- p.html[1] %>% html_text()
+      }
+    } else {
+      return()
+    }
+    
+    length.comments = nrow(comment)
+    if(length.comments > 1){
+      for(h in 1:length.comments){
+        if(grepl(comment.p, comment$text[h], fixed=TRUE)){
+          mention <- comment$author[h]
+          mentions = c(mentions,mention)
         }
       }
-      
+    } else if(length.comments == 1){
+        if(grepl(comment.p, comment$text, fixed=TRUE)){
+          mention <- comment$author
+          mentions = c(mentions,mention)
+        }
     }
+      
   }
+  return(mentions)
 }
--- TurnoverAnalysis10.R
+++ TurnoverAnalysis10.R
@@ -3,19 +3,16 @@ library(rvest)#https://www.datacamp.com/community/tutorials/r-web-scraping-rvest
 library(purrr)#function map to get unique element html
 # lapply(dbListConnections(MySQL()), dbDisconnect)
 
-#geração da rede social
+#geraÃ§Ã£o da rede social
 
-
-
-getInteractions <- function(name, owner, query){
-  
-  rs = dbSendQuery(mydb, query)
-  rows = fetch(rs, n=-1)
-  rows$CREATEDAT = as.Date(rows$CREATEDAT)
-  return(na.omit(rows))
+create.data.network <-function(){
+  projects <- read.csv(file = "input/repositories.csv", sep = ",")
+  for(i in 1:nrow(projects)){
+    create.social.network(projects[i,]$name, projects[i,]$owner)
+  }
 }
 
-getNetwork <- function(){
+create.metric.network <- function(){
   name = "freeCodeCamp";
   owner = "freeCodeCamp";
   
@@ -27,20 +24,6 @@ getNetwork <- function(){
   for ( i in seq_along(days) )
   {
     print(paste(days[i],"T12:00:00", sep=""))
-  }
-}
-
-setMentions <- function(){
-  name = "freeCodeCamp";
-  owner = "freeCodeCamp";
-  
-  issues = getInteractions(name, owner, getQueryIssue(name, owner));
-  issuescomment = getInteractions(name, owner, getQueryIssueComment(name, owner));
-  interactions = rbind(issues, issuescomment);
-  for(row in 1:nrow(issues)){
-    id = issues[row]$ID;
-    mentions = issues[row]$mention
-    print(id);
   }
 }
 
